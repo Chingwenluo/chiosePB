fdb359b8aeb7fd8b7a8295210166564d9cff1cbd
ChingWen <fsvnxwx@gmail.com>
2020-08-24 23:06:11 +0800

    Initial Commit

---
+++ chiosePB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chiosePB
+//
+//  Created by eric on 2020/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
